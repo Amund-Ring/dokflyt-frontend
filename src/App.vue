a510a4ff41e4897676983aba57acd9ab3cd87b94
Amund Ring <60844467+Amund-Ring@users.noreply.github.com>
2023-01-07 13:57:52 +0100

    Dropdown menu component

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,28 @@
 <template>
-
-  
-  
   <div class="flex h-full min-h-screen w-screen flex-col items-center bg-blue-100 py-32">
-
-    <DropdownMenu :options="raceOptions" />
-
-
     <div class="w-9/12 overflow-hidden rounded-lg border-2 border-slate-300">
-      <div class="w-full border-b border-slate-400 bg-white p-6">
+      <div class="flex h-24 w-full gap-x-4 border-b border-slate-400 bg-white p-6">
         <input
           type="text"
           v-model="search"
           placeholder="🔎   Search"
-          class="w-36 rounded-lg border-2 border-slate-300 px-3 py-2 text-center text-sm"
+          class="w-36 rounded-lg border-2 border-slate-300 px-3 text-center text-sm outline-none focus:border-blue-300 focus:ring-blue-100"
           onfocus="this.placeholder=''"
           onblur="this.placeholder='🔎   Search name'"
         />
 
-        
+        <DropdownMenu v-model="selectedRace" :options="raceOptions" />
 
+        <!-- <select
+          v-model="selectedRace"
+          class="h-full w-full rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 text-center text-sm text-gray-900 focus:border-blue-300 focus:ring-blue-100"
+        >
+          <option v-for="option in raceOptions" :value="option">
+            {{ option }}
+          </option>
+        </select> -->
+
+        <div>Selected race: {{ selectedRace }}</div>
       </div>
 
       <CharacterTable :characters="filteredCharacters" />
@@ -51,15 +54,33 @@
       return {
         search: '',
         characters: characterData,
-        raceOptions: ['Human', 'Dwarf', 'Elf'],
-        selectedRace: ''
+        // raceOptions: ['Human', 'Dwarf', 'Elf'],
+        selectedRace: null
       };
     },
     computed: {
       filteredCharacters(): Character[] {
-        return this.characters.filter(character =>
-          character.name.toLowerCase().includes(this.search.toLowerCase())
-        );
+        let filteredChars = this.characters;
+
+        if (this.selectedRace) {
+          filteredChars = filteredChars.filter(char => char.race === this.selectedRace)
+        }
+
+        if (this.search) {
+          return filteredChars.filter(character =>
+            character.name.toLowerCase().includes(this.search.toLowerCase())
+          );
+        }
+
+        return filteredChars;
+      },
+      raceOptions(): String[] {
+        const races: String[] = [];
+        this.characters.forEach(character => {
+          races.push(character.race);
+        });
+        const racesWithoutDuplicates = [...new Set(races)];
+        return racesWithoutDuplicates.sort();
       }
     }
   });
--- src/components/DropdownMenu.vue
+++ src/components/DropdownMenu.vue
@@ -1,12 +1,17 @@
-<template>
-  <div>
-    <select >
-
-      <option v-for="option in options" :value="option">{{ option }}</option>
+<template class>
+  <div class="h-full grow">
+    <select
+      value="modelValue"
+      @input="$emit('update:modelValue', ($event.target as HTMLInputElement).value)"
+      class="h-full w-full rounded-lg border-2 border-gray-300 bg-gray-50 p-2.5 text-center text-sm text-gray-900 focus:border-blue-300 focus:ring-blue-100"
+    >
+      <!-- <option :value="null" disabled>Select Race</option> -->
+      <option v-for="option in options" :value="option">
+        {{ option }}
+      </option>
     </select>
   </div>
 </template>
-
 
 <script lang="ts">
   import { defineComponent } from 'vue';
@@ -17,7 +22,8 @@
       options: {
         type: Array as any,
         required: true
-      }
+      },
+      modelValue: String
     }
   });
 </script>
